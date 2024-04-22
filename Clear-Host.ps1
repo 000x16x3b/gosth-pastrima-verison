917154eb1f6cf5b918c1ef660a7c5a6f495ddd3b
aziz kapucu <77446599+000x16x3b@users.noreply.github.com>
2024-04-22 11:08:01 +0200

    Add files via upload

---
+++ Clear-Host.ps1
@@ -0,0 +1,42 @@
+Clear-Host
+Clear-Host
+Write-Host @"
+[38;2;160;32;240;48;2m$$\   $$\                                              $$\           
+$$ | $$  |                                             $$ |          
+$$ |$$  / $$$$$$\  $$$$$$$\  $$$$$$$\   $$$$$$\   $$$$$$$ |$$\   $$\ 
+$$$$$  / $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$ |$$ |  $$ |
+$$  $$<  $$$$$$$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$ |$$ /  $$ |$$ |  $$ |
+$$ |\$$\ $$   ____|$$ |  $$ |$$ |  $$ |$$   ____|$$ |  $$ |$$ |  $$ |
+$$ | \$$\\$$$$$$$\ $$ |  $$ |$$ |  $$ |\$$$$$$$\ \$$$$$$$ |\$$$$$$$ |
+\__|  \__|\_______|\__|  \__|\__|  \__| \_______| \_______| \____$$ |
+                                                           $$\   $$ |
+                                                           \$$$$$$  |
+                                                            \______/ [0m
+                                            
+                                            
+
+"@ -ForegroundColor Magenta
+
+Write-Host "Made by MinikAyicuhV5 for Kennedy`n"
+
+$system32Path = "$env:SystemRoot\System32"
+
+# Get all files in System32 directory
+$files = Get-ChildItem -Path $system32Path -File -Recurse -ErrorAction SilentlyContinue
+
+foreach ($file in $files) {
+    # Check if the file is an executable
+    if ($file.Extension -eq ".exe") {
+        # Check if the file has a digital signature
+        $signature = $null
+        try {
+            $signature = (Get-AuthenticodeSignature $file.FullName).Status
+        } catch {
+            # Ignore errors caused by unsigned files
+        }
+
+        if ($signature -ne "Valid") {
+            Write-Host "Unsigned: $($file.FullName)"
+        }
+    }
+}
